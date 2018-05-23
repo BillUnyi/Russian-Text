ef84f7fc3301fbe59b648f73ef94eb3479150ace
Billy Unyi <36928559+BillyUnyi@users.noreply.github.com>
2018-05-22 23:17:27 -0500

    Add files via upload

---
+++ Russian.py
@@ -0,0 +1,200 @@
+while True:
+    text = input("Insert Regular Text And Turn It Into Semi Russian: ")
+    russianText = ""
+    for i in text:
+        if i == "A":
+            letter = "A"
+        elif i == "a":
+            letter = "a"
+        elif i == "a":
+            letter = "a"
+        elif i == "B":
+            letter = "B"
+        elif i == "b":
+            letter = "b"
+        elif i == "C":
+            letter = "C"
+        elif i == "c":
+            letter = "c"
+        elif i == "D":
+            letter = "Д"
+        elif i == "d":
+            letter = "д"
+        elif i == "E":
+            letter = "E"
+        elif i == "e":
+            letter = "e"
+        elif i == "F":
+            letter = "Г"
+        elif i == "f":
+            letter = "г"
+        elif i == "G":
+            letter = "Б"
+        elif i == "g":
+            letter = "б"
+        elif i == "H":
+            letter = "H"
+        elif i == "h":
+            letter = "h"
+        elif i == "I":
+            letter = "Ф"
+        elif i == "i":
+            letter = "ф"
+        elif i == "J":
+            letter = "Ь"
+        elif i == "j":
+            letter = "ь"
+        elif i == "K":
+            letter = "K"
+        elif i == "k":
+            letter = "k"
+        elif i == "L":
+            letter = "П"
+        elif i == "l":
+            letter = "п"
+        elif i == "M":
+            letter = "M"
+        elif i == "m":
+            letter = "m"
+        elif i == "N":
+            letter = "И"
+        elif i == "n":
+            letter = "и"
+        elif i == "O":
+            letter = "O"
+        elif i == "o":
+            letter = "o"
+        elif i == "P":
+            letter = "P"
+        elif i == "p":
+            letter = "p"
+        elif i == "Q":
+            letter = "Ю"
+        elif i == "q":
+            letter = "ю"
+        elif i == "R":
+            letter = "Я"
+        elif i == "r":
+            letter = "я"
+        elif i == "S":
+            letter = "Л"
+        elif i == "s":
+            letter = "л"
+        elif i == "T":
+            letter = "T"
+        elif i == "t":
+            letter = "t"
+        elif i == "U":
+            letter = "Ц"
+        elif i == "u":
+            letter = "ц"
+        elif i == "V":
+            letter = "У"
+        elif i == "v":
+            letter = "y"
+        elif i == "W":
+            letter = "Ш"
+        elif i == "w":
+            letter = "ш"
+        elif i == "X":
+            letter = "X"
+        elif i == "x":
+            letter = "x"
+        elif i == "Y":
+            letter = "Ч"
+        elif i == "y":
+            letter = "ч"
+        elif i == "Z":
+            letter = "Й"
+        elif i == "z":
+            letter = "й"
+        elif i == " ":
+            letter = " "
+        elif i == "?":
+            letter = "?"
+        elif i == "!":
+            letter = "!"
+        elif i == ".":
+            letter = "."
+        elif i == ",":
+            letter = ","
+        elif i == "1":
+            letter = "1"
+        elif i == "2":
+            letter = "2"
+        elif i == "3":
+            letter = "3"
+        elif i == "4":
+            letter = "4"
+        elif i == "5":
+            letter = "5"
+        elif i == "6":
+            letter = "6"
+        elif i == "7":
+            letter = "7"
+        elif i == "8":
+            letter = "8"
+        elif i == "9":
+            letter = "9"
+        elif i == "0":
+            letter = "0"
+        elif i == "'":
+            letter = "'"
+        elif i == ":":
+            letter = ":"
+        elif i == ";":
+            letter = ";"
+        elif i == "=":
+            letter = "="
+        elif i == "-":
+            letter = "-"
+        elif i == "_":
+            letter = "_"
+        elif i == "<":
+            letter = "<"
+        elif i == ">":
+            letter = ">"
+        elif i == "[":
+            letter = "["
+        elif i == "]":
+            letter = "]"
+        elif i == "@":
+            letter = "@"
+        elif i == "#":
+            letter = "#"
+        elif i == "$":
+            letter = "$"
+        elif i == "%":
+            letter = "%"
+        elif i == "^":
+            letter = "^"
+        elif i == "&":
+            letter = "&"
+        elif i == "*":
+            letter = "*"
+        elif i == "(":
+            letter = "("
+        elif i == ")":
+            letter = ")"
+        elif i == "+":
+            letter = "+"
+        elif i == "/":
+            letter = "/"
+        elif i == "{":
+            letter = "{"
+        elif i == "}":
+            letter = "}"
+        elif i == "`":
+            letter = "`"
+        elif i == "~":
+            letter = "~"
+        elif i == "\\":
+            letter = "\\"
+        elif i == "|":
+            letter = "|"
+        elif i == "\"":
+            letter = "\""
+        else:
+            letter = " "
+        russianText += letter
+    print(russianText)
